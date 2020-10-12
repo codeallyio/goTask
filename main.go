da90e3b056a99882a8aa74ea516d358577bcffc3
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-10-13 00:24:47 +0200

    [fix] task description in .go files

--- main.go
+++ main.go
@@ -2,8 +2,8 @@
 
 package main
 
-// connection's holdUntil time is being set to 3 seconds when getting connected
-// implement function, checking if connection's holdUntil time exceeded current time
+// Connection's holdUntil time is being set to 3 seconds when getting connected.
+// Implement function, checking if connection's holdUntil time exceeded current time.
 func (c *connection) isActive() bool {
 	return false
 }
@@ -12,6 +12,6 @@ func main() {
 	runMockServer()
 	defer checkSuccess()
 
-	// handle here all requests located in activeRequest channel
-	// use `handleRequest(r request)` function
+	// Here handle all the requests from activeRequest channel.
+	// For this use the `handleRequest(r request)` function.
 }
